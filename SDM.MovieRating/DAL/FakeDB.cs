5eacc4193a100c76ff3571018dfe37f262916766
GustavoAmericano <quizandk@gmail.com>
2018-10-25 13:51:46 +0200

    Added test for init data
    Reworked a lot of stuff, moved from objects to dictionaries. Data is now fetched on FakeDB init (Constructor)

--- SDM.MovieRating/DAL/FakeDB.cs
+++ SDM.MovieRating/DAL/FakeDB.cs
@@ -1,7 +1,36 @@
-﻿namespace SDM.MovieRating.DAL
+﻿using System.Collections.Generic;
+using SDM.MovieRating.BE;
+
+namespace SDM.MovieRating.DAL
 {
     public class FakeDB
     {
-        
+        public List<MovieReview> MovieReviews { get; set; }
+        public Dictionary<int, List<MovieReview>> Movies;
+        public Dictionary<int, List<MovieReview>> Reviewers;
+
+        public FakeDB()
+        {
+            Movies = new Dictionary<int, List<MovieReview>>();
+            Reviewers = new Dictionary<int, List<MovieReview>>();
+            JSONReader jr = new JSONReader();
+            MovieReviews = jr.ReadJSON();
+
+            InsertIntoDictionaries();
+        }
+
+        private void InsertIntoDictionaries()
+        {
+            foreach (MovieReview m in MovieReviews)
+            {
+                if (!Reviewers.ContainsKey(m.ReviewerId))
+                    Reviewers[m.ReviewerId] = new List<MovieReview>();
+                Reviewers[m.ReviewerId].Add(m);
+
+                if (!Movies.ContainsKey(m.MovieId))
+                    Movies[m.MovieId] = new List<MovieReview>();
+                Movies[m.MovieId].Add(m);
+            }
+        }
     }
 }
--- XUnitTestProject1/DAL/FakeDBTest.cs
+++ XUnitTestProject1/DAL/FakeDBTest.cs
@@ -1,7 +1,17 @@
-﻿namespace MovieRatingTest.DAL
+﻿using SDM.MovieRating.DAL;
+using Xunit;
+
+namespace MovieRatingTest.DAL
 {
     public class FakeDBTest
     {
-        
+        [Fact]
+        public void TestDataExistOnInitializationSmileyFace()
+        {
+            FakeDB db = new FakeDB();
+            Assert.NotEmpty(db.MovieReviews);
+            Assert.NotEmpty(db.Movies);
+            Assert.NotEmpty(db.Reviewers);
+        }
     }
 }
